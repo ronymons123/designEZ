e7fed7a038340aba72487d93c19e2356ecbb4290
RONY MONS <ronymons109@gmail.com>
2023-11-27 19:58:08 +0530

    wallcreation

---
+++ Assets/CreateWalls.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateWalls : MonoBehaviour
+{
+    bool creating;
+    public GameObject start;
+    public GameObject end;
+
+    public GameObject wallprefab;
+    GameObject wall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getInput();
+        
+    }
+
+    void getInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            setStart();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            setEnd();
+        }
+        else
+        {
+            if (creating)
+            {
+                adjust();
+            }
+
+        }
+    }
+
+        void setStart()
+        {
+            creating = true;
+            start.transform.position = getWordPoint();
+            wall=(GameObject) Instantiate(wallprefab,start.transform.position,Quaternion.identity);
+        }
+        void setEnd()
+        {
+            creating = false;
+            end.transform.position = getWordPoint();
+    }
+        void adjust()
+        {
+            end.transform.position = getWordPoint();
+            adjustWall();
+    }
+    void adjustWall()
+    {
+        start.transform.LookAt(end.transform.position);
+        start.transform.LookAt(start.transform.position);
+        float distance = Vector3.Distance(start.transform.position, end.transform.position);
+        wall.transform.position=start.transform.position+ distance/2* start.transform.forward;
+        wall.transform.rotation=start.transform.rotation;
+        wall.transform.localScale = new Vector3(wall.transform.localScale.x, wall.transform.localScale.y, distance);
+    }
+    Vector3 getWordPoint()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit) )
+        {
+            return hit.point;
+        }
+        return Vector3.zero;
+    }
+}
